92faa92adff67b586048adddd23aa245c487a3c6
Sely <mailmesalah@gmail.com>
2022-09-27 13:33:44 +0100

    First Commit

---
+++ DesktopPharmacyAppWithoutTaxClient/WpfPharmacyAppWithoutTaxClient/Dashboard.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System.Windows;
+using WpfClientApp.Registers;
+using WpfClientApp.Reports;
+using WpfClientApp.Transactions;
+
+namespace WpfClientApp
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void ProductRegister_Click(object sender, RoutedEventArgs e)
+        {
+            ProductRegister pr = new ProductRegister();
+            pr.Show();
+        }        
+
+        private void PurchaseInterstate_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseInterstate pi = new PurchaseInterstate();
+            pi.Show();
+        }
+
+        private void PurchaseWholesale_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseWholesale pw = new PurchaseWholesale();
+            pw.Show();
+        }
+
+        private void SalesInterstate_Click(object sender, RoutedEventArgs e)
+        {
+            SalesInterstate si = new SalesInterstate();
+            si.Show();
+        }
+
+        private void SalesWholesale_Click(object sender, RoutedEventArgs e)
+        {
+            SalesWholesale sw = new SalesWholesale();
+            sw.Show();
+        }
+
+        private void SalesLocal_Click(object sender, RoutedEventArgs e)
+        {
+            SalesLocal sl = new SalesLocal();
+            sl.Show();
+        }
+
+        private void PurchaseReturnInterstate_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseReturnInterstate pri = new PurchaseReturnInterstate();
+            pri.Show();
+        }
+
+        private void PurchaseReturnWholesale_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseReturnWholesale prw = new PurchaseReturnWholesale();
+            prw.Show();
+        }
+
+        private void SalesReturnInterstate_Click(object sender, RoutedEventArgs e)
+        {
+            SalesReturnInterstate sri = new SalesReturnInterstate();
+            sri.Show();
+        }
+
+        private void SalesReturnWholesale_Click(object sender, RoutedEventArgs e)
+        {
+            SalesReturnWholesale srw = new SalesReturnWholesale();
+            srw.Show();
+        }
+
+        private void SalesReturnLocal_Click(object sender, RoutedEventArgs e)
+        {
+            SalesReturnLocal srl = new SalesReturnLocal();
+            srl.Show();
+        }
+
+        private void StockAddition_Click(object sender, RoutedEventArgs e)
+        {
+            StockAddition sa = new StockAddition();
+            sa.Show();
+        }
+
+        private void StockDeletion_Click(object sender, RoutedEventArgs e)
+        {
+            StockDeletion sd = new StockDeletion();
+            sd.Show();
+        }
+
+        private void SupplierRegisters_Click(object sender, RoutedEventArgs e)
+        {
+            SupplierRegisters sr = new SupplierRegisters();
+            sr.Show();
+        }
+
+        private void CustomerRegisters_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerRegisters cr = new CustomerRegisters();
+            cr.Show();
+        }
+
+        private void PurchaseInterstateReport_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseInterstateReport pir = new PurchaseInterstateReport();
+            pir.Show();
+        }
+
+        private void PurchaseWholesaleReport_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseWholesaleReport pwr = new PurchaseWholesaleReport();
+            pwr.Show();
+        }
+
+        private void SalesInterstateReport_Click(object sender, RoutedEventArgs e)
+        {
+            SalesInterstateReport sir = new SalesInterstateReport();
+            sir.Show();
+        }
+
+        private void SalesWholesaleReport_Click(object sender, RoutedEventArgs e)
+        {
+            SalesWholesaleReport swr = new SalesWholesaleReport();
+            swr.Show();
+        }
+
+        private void SalesLocalReport_Click(object sender, RoutedEventArgs e)
+        {
+            SalesLocalReport slr = new SalesLocalReport();
+            slr.Show();
+        }
+
+        private void PurchaseReturnInterstateReport_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseReturnInterstateReport prir = new PurchaseReturnInterstateReport();
+            prir.Show();
+        }
+
+        private void PurchaseReturnWholesaleReport_Click(object sender, RoutedEventArgs e)
+        {
+            PurchaseReturnWholesaleReport prwr = new PurchaseReturnWholesaleReport();
+            prwr.Show();
+        }
+
+        private void SalesReturnInterstateReport_Click(object sender, RoutedEventArgs e)
+        {
+            SalesReturnInterstateReport srir = new SalesReturnInterstateReport();
+            srir.Show();
+        }
+
+        private void SalesReturnWholesaleReport_Click(object sender, RoutedEventArgs e)
+        {
+            SalesReturnWholesaleReport srwr = new SalesReturnWholesaleReport();
+            srwr.Show();
+        }
+
+        private void SalesReturnLocalReport_Click(object sender, RoutedEventArgs e)
+        {
+            SalesReturnLocalReport srlr = new SalesReturnLocalReport();
+            srlr.Show();
+        }
+
+        private void StockAdditionReport_Click(object sender, RoutedEventArgs e)
+        {
+            StockAdditionReport sar = new StockAdditionReport();
+            sar.Show();
+        }
+
+        private void StockDeletionReport_Click(object sender, RoutedEventArgs e)
+        {
+            StockDeletionReport sdr = new StockDeletionReport();
+            sdr.Show();
+        }
+    }
+}
---
+++ DesktopPharmacyAppWithoutTaxServer/WpfPharmacyAppWithoutTaxServer/Dashboard.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfServerApp.Services;
+
+namespace WpfServerApp
+{
+    
+    public partial class MainWindow : Window
+    {
+        ServiceHost hostLedgerService;
+        ServiceHost hostBillNoService;
+        ServiceHost hostUnitService;
+        ServiceHost hostProductService;
+        ServiceHost hostPurchaseService;
+        ServiceHost hostPurchaseReturnService;
+        ServiceHost hostSalesService;
+        ServiceHost hostSalesReturnService;
+        ServiceHost hostStockAdditionService;
+        ServiceHost hostStockDeletionService;        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Initialising host object
+            hostLedgerService = new ServiceHost(typeof(Services.LedgerService));
+            hostBillNoService = new ServiceHost(typeof(Services.BillNoService));            
+            hostUnitService = new ServiceHost(typeof(Services.UnitService));
+            hostProductService = new ServiceHost(typeof(Services.ProductService));
+            hostPurchaseService = new ServiceHost(typeof(Services.PurchaseService));
+            hostPurchaseReturnService = new ServiceHost(typeof(Services.PurchaseReturnService));
+            hostSalesService = new ServiceHost(typeof(Services.SalesService));
+            hostSalesReturnService = new ServiceHost(typeof(Services.SalesReturnService));
+            hostStockAdditionService = new ServiceHost(typeof(Services.StockAdditionService));
+            hostStockDeletionService = new ServiceHost(typeof(Services.StockDeletionService));            
+
+            hostBillNoService.Open();
+            hostLedgerService.Open();
+            hostUnitService.Open();
+            hostProductService.Open();
+            hostPurchaseService.Open();
+            hostPurchaseReturnService.Open();
+            hostSalesService.Open();
+            hostSalesReturnService.Open();
+            hostStockAdditionService.Open();
+            hostStockDeletionService.Open();            
+
+            //Loading the Unique Ledgers
+            LedgerService ls = new LedgerService();
+            ls.LoadAllUniqueLedgers();
+            //Loading barcode characters
+            BarcodeService bs = new BarcodeService();
+            bs.initialiseBarcodeService();
+
+            Console.WriteLine("Services are started and running");
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //Closing the service hoster
+            hostLedgerService.Close();
+            hostBillNoService.Close();
+            hostUnitService.Close();
+            hostProductService.Close();
+            hostPurchaseService.Close();
+            hostPurchaseReturnService.Close();
+            hostSalesService.Close();
+            hostSalesReturnService.Close();
+            hostStockAdditionService.Close();
+            hostStockDeletionService.Close();            
+
+            Console.WriteLine("Services are stopped");
+        }
+    }
+}
